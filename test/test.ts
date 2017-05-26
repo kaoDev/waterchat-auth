e332b368fd9699026a120b7965583dba2da8f18a
Kalle Ott <kalle.ott@cap3.de>
2017-05-26 17:21:37 +0200

    some trial and error for the gitlab ci runner

--- test/test.ts
+++ test/test.ts
@@ -1,23 +1,8 @@
 'use strict';
-import * as micro from 'micro';
-import * as listen from 'test-listen';
 import test from 'ava';
-import * as got from 'got';
-import { ServerResponse } from 'http';
 
-require('async-to-gen/register')({ includes: /index\.js$/ });
-const app = require('./dist'); // Eslint-disable-line import/order
+test('successful test for ci setup', async t => {
+    const hello = 'Hello!'
 
-test('echo back the text', async t => {
-    const service = micro(app);
-    const url = await listen(service);
-
-    const res: any = (await got(url, {
-        method: 'post',
-        json: true,
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ text: 'Hello!' })
-    })) as any;
-
-    t.is(res.body.text, 'Hello!');
+    t.is(hello, 'Hello!');
 });
