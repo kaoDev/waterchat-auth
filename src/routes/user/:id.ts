18901ef801c8039402947bb8ae5b9d5f380ad3f6
Kalle Ott <kalle.ott@cap3.de>
2017-06-08 17:05:46 +0200

    fixed user get

--- src/routes/user/:id.ts
+++ src/routes/user/:id.ts
@@ -1,22 +1,32 @@
 import { send } from 'micro'
 import { IncomingMessage, ServerResponse } from 'http'
 import { userState } from '../../persistence/eventStore'
+import { isSessionValid } from '../../logic/SessionFunctions'
 import { displayUser } from '../../model/User'
 
 export const GET = async (req: IncomingMessage, res: ServerResponse) => {
   try {
-    const userId = (req as any).params.id as string
+    const id = (req as any).params.id as string
 
-    const user = await userState
-      .take(1)
-      .flatMap(s => s.users)
-      .find(u => u.userId === userId)
-      .toPromise()
+    if (await isSessionValid(id)) {
+      const user = await userState
+        .take(1)
+        .map(s => s.sessions[id].user)
+        .toPromise()
 
-    if (user !== undefined) {
       send(res, 200, displayUser(user))
-    } else {
-      send(res, 404)
+    } else if (id !== undefined && id !== null && id.length > 0) {
+      const user = await userState
+        .take(1)
+        .flatMap(s => s.users)
+        .find(u => u.userId === id)
+        .toPromise()
+
+      if (user !== undefined) {
+        send(res, 200, displayUser(user))
+      } else {
+        send(res, 404)
+      }
     }
   } catch (err) {
     console.error('isSessionValid', 'SOMETHING WENT WRONG', err)
